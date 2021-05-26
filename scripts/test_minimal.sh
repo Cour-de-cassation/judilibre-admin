cc1704c538a110c248d5ed5ac27834b844c1767e
antoinefa <fabien.antoine@m4x.org>
2021-05-27 00:17:11 +0200

    👷🐳☸️  CI/CD workflow still to be tested

--- scripts/test_minimal.sh
+++ scripts/test_minimal.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo Test GET routes on ${API_SCHEME}://${API_HOST}:${API_PORT}
 for route in admin;do
-  if curl -s --retry 5 --retry-delay 2 -XGET ${SCHEME}://${HOST}:${API_PORT}/${route} | grep -q '"route":"GET /'${route}'"' ; then
+  if curl -s --retry 5 --retry-delay 2 -XGET ${API_SCHEME}://${API_HOST}:${API_PORT}/${route} | grep -q '"route":"GET /'${route}'"' ; then
       echo "✅ ${route}"
   else
       echo -e "\e[31m❌ ${route} !\e[0m"
