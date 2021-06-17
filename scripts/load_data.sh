267da9a70765ab9ac6489b8d61318f6e1f736324
antoinefa <fabien.antoine@m4x.org>
2021-06-17 02:39:40 +0200

    🐛 fix missing field "type" for injection tests

--- scripts/load_data.sh
+++ scripts/load_data.sh
@@ -15,7 +15,7 @@ fi;
 : ${IMPORT_MSG:=injecting}
 rm -rf .bulk > /dev/null 2>&1
 mkdir .bulk
-(find ${DATA_DIR}/ -type f | head -${IMPORT_LIMIT} | xargs jq -c '.' \
+(find ${DATA_DIR}/ -type f | head -${IMPORT_LIMIT} | xargs jq -c '. + {"type":"arret"}' \
     | awk 'BEGIN{l=0;s=1;b=""}{if (b==""){b=$0}else{b=b "," $0};if (s == '"${IMPORT_SIZE}"') {print "{\"id\": \"" l++ "\", \"decisions\": [" b "]}";s=0;b=""};s++}END{if (b!=""){print "{\"id\": \"" l++ "\", \"decisions\": [" b "]}"}}' \
     | split -l1 - .bulk/lot_decisions_ && echo -n  "✓   prepared batch of size ${IMPORT_SIZE}") || \
     (echo -e "e[31m❌ preparation of batch failed" && exit 1);
