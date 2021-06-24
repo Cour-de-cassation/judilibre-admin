ad0aec1e76a64f5aad55249e79f061e866bf5cf8
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2021-06-24 11:52:06 +0200

    Setting up for unit tests.

--- src/modules/elastic.js
+++ src/modules/elastic.js
@@ -1,9 +1,11 @@
 require('./env');
-const { Client } = require('@elastic/elasticsearch');
 
 class Elastic {
   constructor() {
-    this.client = new Client({ node: `${process.env.ELASTIC_NODE}`, ssl: { rejectUnauthorized: false }});
+    if (!process.env.WITHOUT_ELASTIC) {
+      const { Client } = require('@elastic/elasticsearch');
+      this.client = new Client({ node: `${process.env.ELASTIC_NODE}`, ssl: { rejectUnauthorized: false } });
+    }
   }
 }
 
