26efc797081a0d0beee19ee8c7a7c4e2d599ca12
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2021-06-24 11:43:28 +0200

    Starting to add commands to the `/admin` route: `delete_all`, `refresh_template`.

--- src/api/admin.js
+++ src/api/admin.js
@@ -1,30 +1,74 @@
 require('../modules/env');
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const api = express.Router();
+const { checkSchema, validationResult } = require('express-validator');
+const Elastic = require('../modules/elastic');
 const route = 'admin';
+const commands = ['delete_all', 'refresh_template'];
 
-api.get(`/${route}`, async (req, res) => {
-  try {
-    const result = await getAdmin(req.query);
-    if (result.errors) {
-      return res.status(400).json({
-        route: `${req.method} ${req.path}`,
-        errors: result.errors,
-      });
+api.get(
+  `/${route}`,
+  checkSchema({
+    command: {
+      in: 'query',
+      isString: true,
+      toLowerCase: true,
+      isIn: {
+        options: [commands],
+      },
+      errorMessage: `The command parameter is required and its value must be in [${commands}].`,
+      optional: false,
+    },
+  }),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ route: `${req.method} ${req.path}`, errors: errors.array() });
     }
-    return res.status(200).json(result);
-  } catch (e) {
-    return res
-      .status(500)
-      .json({ route: `${req.method} ${req.path}`, errors: [{ msg: 'Internal Server Error', error: e.message }] });
-  }
-});
+    try {
+      const result = await getAdmin(req.query);
+      if (result.errors) {
+        return res.status(400).json({
+          route: `${req.method} ${req.path}`,
+          errors: result.errors,
+        });
+      }
+      return res.status(200).json(result);
+    } catch (e) {
+      console.log(e.meta.body);
+      return res
+        .status(500)
+        .json({ route: `${req.method} ${req.path}`, errors: [{ msg: 'Internal Server Error', error: e.message }] });
+    }
+  },
+);
 
 async function getAdmin(query) {
-  return {
+  const response = {
     route: `GET /${route}`,
-    query: query,
+    command: query.command,
+    result: null,
   };
+  switch (query.command) {
+    case 'delete_all':
+      const deleteResult = await Elastic.client.indices.delete({
+        index: process.env.ELASTIC_INDEX,
+      });
+      response.result = deleteResult.body;
+      break;
+    case 'refresh_template':
+      const template = JSON.parse(fs.readFileSync(path.join(__dirname, '..', '..', 'elastic', 'template-medium.json')));
+      const refreshResult = await Elastic.client.indices.putTemplate({
+        name: 't_judilibre',
+        create: false,
+        body: template,
+      });
+      response.result = refreshResult.body;
+      break;
+  }
+  return response;
 }
 
 module.exports = api;
