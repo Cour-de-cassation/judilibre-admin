1080b2bf86332675ee4dc9c3d9a9aa6c416c19d5
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2021-09-08 23:27:14 +0200

    Add new command to the list of available commands.

--- src/api/admin.js
+++ src/api/admin.js
@@ -6,7 +6,7 @@ const api = express.Router();
 const { checkSchema, validationResult } = require('express-validator');
 const Elastic = require('../modules/elastic');
 const route = 'admin';
-const commands = ['delete_all', 'refresh_template', 'test'];
+const commands = ['delete_all', 'refresh_template', 'show_template', 'test'];
 
 api.get(
   `/${route}`,
