1821951831cd7b582f8abd4136aec7577590acfe
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2021-10-01 16:09:11 +0200

    Add /healthcheck

---
+++ src/api/healthcheck.js
@@ -0,0 +1,32 @@
+require('../modules/env');
+const express = require('express');
+const api = express.Router();
+const Elastic = require('../modules/elastic');
+const route = 'healthcheck';
+
+api.get(`/${route}`, async (req, res) => {
+  try {
+    const result = await getHealthcheck(req.query);
+    return res.status(200).json(result);
+  } catch (e) {
+    return res.status(200).json({
+      status: 'indisponible',
+      reason: e.message,
+    });
+  }
+});
+
+async function getHealthcheck(query) {
+  const ping = await Elastic.client.ping({});
+  if (ping.body === true && ping.statusCode === 200) {
+    return {
+      status: 'disponible',
+    };
+  } else {
+    return {
+      status: 'indisponible',
+    };
+  }
+}
+
+module.exports = api;
--- src/api/index.js
+++ src/api/index.js
@@ -3,5 +3,6 @@ const apis = [];
 apis.push(require('./admin'));
 apis.push(require('./import'));
 apis.push(require('./delete'));
+apis.push(require('./healthcheck'));
 
 module.exports = apis;
