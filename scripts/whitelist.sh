2e0b402339ba9b5b79d95e2256430101ad96abfb
antoinefa <fabien.antoine@m4x.org>
2021-11-01 14:52:02 +0100

    👷 update CI IPs

--- scripts/whitelist.sh
+++ scripts/whitelist.sh
@@ -5,6 +5,6 @@
     curl -s https://uptimerobot.com/inc/files/ips/IPv4andIPv6.txt | grep -v '::' | tr '\n' ',' #uptime-robot
     echo 185.24.185.46/27 #piste
     echo 185.24.186.214 #mj
-    echo 78.192.252.15,163.172.185.238 #fa
+    echo 78.192.252.15,163.172.185.238,163.172.130.121 #fa
     echo -n 80.87.224.00/22 #actimage
 ) | tr '\n' ','
