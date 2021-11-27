849e13568f997075033c366988bdc34069f095bb
antoinefa <fabien.antoine@m4x.org>
2021-11-27 17:30:17 +0100

    👷 restore node package version to docker version

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-cat tagfiles.version | xargs -I '{}' find {} -type f | egrep -v '(.tar.gz)$' | sort | xargs cat | sha256sum - | sed 's/\(......\).*/\1/'
+./scripts/check_install.sh > /dev/null 2>&1
+
+echo $(cat package.json | jq -r '.version')-$(cat tagfiles.version | xargs -I '{}' find {} -type f | egrep -v '(.tar.gz)$' | sort | xargs cat | sha256sum - | sed 's/\(......\).*/\1/')
