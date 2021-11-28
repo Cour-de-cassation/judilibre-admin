6aa1ae8318cb0b445c8b6724d57168abc411c259
antoinefa <fabien.antoine@m4x.org>
2021-11-28 20:54:57 +0100

    ⬆️  upgrade eck operator to 1.8.0

--- scripts/deploy_k8s_services.sh
+++ scripts/deploy_k8s_services.sh
@@ -229,7 +229,7 @@ fi;
 if (${KUBECTL} get elasticsearch >> ${KUBE_INSTALL_LOG} 2>&1); then
         echo "✓   elasticsearch k8s controller";
 else
-        if ( (${KUBECTL} create -f https://download.elastic.co/downloads/eck/1.7.0/crds.yaml && ${KUBECTL} apply -f https://download.elastic.co/downloads/eck/1.7.0/operator.yaml) >> ${KUBE_INSTALL_LOG} 2>&1); then
+        if ( (${KUBECTL} create -f https://download.elastic.co/downloads/eck/1.8.0/crds.yaml && ${KUBECTL} apply -f https://download.elastic.co/downloads/eck/1.8.0/operator.yaml) >> ${KUBE_INSTALL_LOG} 2>&1); then
                 echo "🚀  elasticsearch k8s controller";
         else
                 echo -e "\e[31m❌  elasticsearch k8s controller install failed" && exit 1;
