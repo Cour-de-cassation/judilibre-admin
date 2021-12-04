24a9f01e62f8f0888ac3a868906cbd48c650434c
antoinefa <fabien.antoine@m4x.org>
2021-12-04 10:10:03 +0100

    add self signed certificate generation

---
+++ scripts/generate-certificate.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+
+if [ -z "${APP_HOST}" ];then
+     echo "\e[31m❌  APP_HOST must be in env\e[0m" && exit 1;
+fi;
+
+if [ ! -f server.crt -o ! -f server.key ];then
+    echo "▶️   Self-signed cert generation"
+    (
+        if ! (which openssl > /dev/null 2>&1);then
+            (which apk 2>&1) && apk add openssl
+            (which apt 2>&1) && apt install openssl
+        fi;
+        export PASS=$(openssl rand -base64 32)
+        openssl genrsa -des3 -passout pass:${PASS} -out server.pass.key 4096
+        openssl rsa -passin pass:${PASS} -in server.pass.key -out server.key
+        rm server.pass.key
+        APP_OU=$(echo ${APP_HOST} | sed 's/[^\.]*.//')
+        openssl req -new -key server.key -out server.csr -subj "/C=FR/ST=Paris/L=Paris/O=cour-de-cassation.justice.fr/OU=${APP_OU}/CN=${APP_HOST}"
+        openssl x509 -req -sha256 -days 365 -in server.csr -signkey server.key -out server.crt
+    ) 2>&1 | stdbuf -o0 awk '{print "    " $0}'
+    echo "✓   Self-signed cert generated"
+fi;
+
