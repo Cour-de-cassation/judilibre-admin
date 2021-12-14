1d73866129b59d0e18d8a0aa541c32d1e6cf0599
antoinefa <fabien.antoine@m4x.org>
2021-12-14 07:18:18 +0100

    👷🐛 fix ubuntu check_install

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-./scripts/check_install.sh > /dev/null 2>&1
+./scripts/check_install.sh > /dev/null
 
 echo $(cat package.json | jq -r '.version')-$(export LC_COLLATE=C;export LC_ALL=C;cat tagfiles.version | xargs -I '{}' find {} -type f | egrep -v '(.tar.gz)$' | sort | xargs cat | sha256sum - | sed 's/\(......\).*/\1/')
