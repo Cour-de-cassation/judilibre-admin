7c5e2056aee6e3cb49c4e5f00bf25240c8b497f8
antoinefa <fabien.antoine@m4x.org>
2021-12-16 22:03:39 +0100

    👷 add custom ENV for judilibre-attachments

--- scripts/deploy_k8s_services.sh
+++ scripts/deploy_k8s_services.sh
@@ -137,18 +137,39 @@ if [ "${APP_GROUP}" == "judilibre-prive" ];then
         if [ -z "${MONGODB_PASSWORD}" ]; then
                 export MONGODB_PASSWORD=$(openssl rand -hex 32)
         fi
-        if [ -z "${INTERNAL_DB_URI}" ]; then
-                export INTERNAL_DB_URI=mongodb://user:${MONGODB_PASSWORD}@mongodb-0.mongodb-svc.${KUBE_NAMESPACE}.svc.cluster.local:27017
-        fi
-        if [ -z "${EXTERNAL_DB_URI}" ]; then
-                export EXTERNAL_DB_URI=mongodb://user:${MONGODB_PASSWORD}@mongodb-0.mongodb-svc.${KUBE_NAMESPACE}.svc.cluster.local:27017
-        fi
-        if [ -z "${INTERNAL_DB_NAME}" ]; then
-                export INTERNAL_DB_NAME=internal
-        fi
-        if [ -z "${EXTERNAL_DB_NAME}" ]; then
-                export EXTERNAL_DB_NAME=external
-        fi
+        if [ "${APP_ID}" == "judilibre-index" ]; then
+                if [ -z "${INTERNAL_DB_URI}" ]; then
+                        export INTERNAL_DB_URI=mongodb://user:${MONGODB_PASSWORD}@mongodb-0.mongodb-svc.${KUBE_NAMESPACE}.svc.cluster.local:27017
+                fi
+                if [ -z "${EXTERNAL_DB_URI}" ]; then
+                        export EXTERNAL_DB_URI=mongodb://user:${MONGODB_PASSWORD}@mongodb-0.mongodb-svc.${KUBE_NAMESPACE}.svc.cluster.local:27017
+                fi
+                if [ -z "${INTERNAL_DB_NAME}" ]; then
+                        export INTERNAL_DB_NAME=internal
+                fi
+                if [ -z "${EXTERNAL_DB_NAME}" ]; then
+                        export EXTERNAL_DB_NAME=external
+                fi
+        elif [ "${APP_ID}" == "judilibre-attachments" ]; then
+                if [ -z "${MONGO_URI}" ]; then
+                        export MONGO_URI=mongodb://user:${MONGODB_PASSWORD}@mongodb-0.mongodb-svc.${KUBE_NAMESPACE}.svc.cluster.local:27017
+                fi
+                if [ -z "${MONGO_DECISIONS_URI}" ]; then
+                        export MONGO_DECISIONS_URI=mongodb://user:${MONGODB_PASSWORD}@mongodb-0.mongodb-svc.${KUBE_NAMESPACE}.svc.cluster.local:27017
+                fi
+                if [ -z "${MONGO_DBNAME}" ]; then
+                        export MONGO_DBNAME=internal
+                fi
+                if [ -z "${MONGO_DECISIONS_DBNAME}" ]; then
+                        export MONGO_DECISIONS_DBNAME=external
+                fi
+                if [ -z "${JWT_SECRET}" ]; then
+                        export JWT_SECRET=$(openssl rand -hex 32)
+                fi
+                if [ -z "${COOKIE_SECRET}" ]; then
+                        export COOKIE_SECRET=$(openssl rand -hex 32)
+                fi;
+        fi;
 fi
 
 
