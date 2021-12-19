91315a49f7f0c0871916c54aea4a8f106f5333be
antoinefa <fabien.antoine@m4x.org>
2021-12-19 14:00:05 +0100

    👷 handles version of python repo with setup.py

--- scripts/version.sh
+++ scripts/version.sh
@@ -2,4 +2,4 @@
 
 ./scripts/check_install.sh > /dev/null
 
-echo $( [ -f "package.json" ] && (cat package.json | jq -r '.version') || git tag )-$(export LC_COLLATE=C;export LC_ALL=C;cat tagfiles.version | xargs -I '{}' find {} -type f | egrep -v '(.tar.gz)$' | sort | xargs cat | sha256sum - | sed 's/\(......\).*/\1/')
+echo $( [ -f "package.json" ] && (cat package.json | jq -r '.version') || [ -f "setup.py" ] && ( grep -r __version__ */__init__.py | sed 's/.*=//;s/"//g;s/\s//g' ) || git tag )-$(export LC_COLLATE=C;export LC_ALL=C;cat tagfiles.version | xargs -I '{}' find {} -type f | egrep -v '(.tar.gz)$' | sort | xargs cat | sha256sum - | sed 's/\(......\).*/\1/')
