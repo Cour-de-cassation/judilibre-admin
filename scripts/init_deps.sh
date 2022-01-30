e65391cd383c1a9813ae4ce45f5824a0c97ac893
antoinefa <fabien.antoine@m4x.org>
2022-01-30 14:24:00 +0100

    move script and k8s confs to judilibre-ops

---
+++ scripts/init_deps.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+export GIT_OPS=judilibre-ops
+export DEPS_SRC=$(pwd)/${GIT_OPS}
+export SCRIPTS_SRC=${DEPS_SRC}/scripts
+export KUBE_SRC=${DEPS_SRC}/k8s
+
+# clone
+if [ ! -d "${SCRIPTS_SRC}" ];then
+    if ! (git clone https://github.com/Cour-de-cassation/${GIT_OPS} > /dev/null 2>&1); then
+        echo -e "\e[31m❌ init failed, couldn't clone git ${GIT_OPS} repository \e[0m" && exit 1;
+        if [ "${GIT_BRANCH}" == "master" ]; then
+            cd ${SCRIPTS_SRC};
+            git checkout master;
+            cd ..;
+        fi;
+    fi;
+fi;
+
+# scripts
+
+for file in $(ls ${SCRIPTS_SRC}); do
+    if [ ! -f "./scripts/$file" ]; then
+        ln -s ${SCRIPTS_SRC}/$file ./scripts/$file;
+    fi;
+done;
+
+# kube configs
+if [ ! -d "k8s/" ];then
+    ln -s ${KUBE_SRC} ./k8s;
+fi;
+
+
+# elastic configs
+if [ ! -d "elastic/" ]; then
+    ln -s ${ELASTIC_SRC} ./elastic;
+fi;
--- scripts/version.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-./scripts/check_install.sh > /dev/null
-
-echo $( ( [ -f "package.json" ] && (cat package.json | jq -r '.version') ) || ( [ -f "setup.py" ] && ( grep -r __version__ */__init__.py | sed 's/.*=//;s/"//g;s/\s//g' ) ) || (git tag | tail -1) )-$(export LC_COLLATE=C;export LC_ALL=C;cat tagfiles.version | xargs -I '{}' find {} -type f | egrep -v '(.tar.gz)$' | sort | xargs cat | sha256sum - | sed 's/\(......\).*/\1/')
