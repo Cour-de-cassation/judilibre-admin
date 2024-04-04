7bda0fc79ee93fec84f3c979039ede372b381eb7
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2024-04-04 13:44:11 +0200

    Add POST /deleteMany

--- src/api/delete.js
+++ src/api/delete.js
@@ -38,6 +38,60 @@ api.post(
   },
 );
 
+api.post(
+  `/${route}Many`,
+  checkSchema({
+    id: {
+      in: 'body',
+      toArray: true,
+    },
+    'id.*': {
+      in: 'body',
+      isString: true,
+      errorMessage: `id  must be an array of strings.`,
+      optional: false,
+    },
+  }),
+  async (req, res) => {
+    const t0 = new Date();
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ route: `${req.method} ${req.path}`, errors: errors.array() });
+    }
+    try {
+      const result = await postDeleteMany(req.body.id);
+      const t1 = new Date();
+      result.took = t1.getTime() - t0.getTime();
+      return res.status(200).json(result);
+    } catch (e) {
+      return res.status(500).json({
+        route: `${req.method} ${req.path}`,
+        errors: [{ msg: 'Internal Server Error', error: JSON.stringify(e, e ? Object.getOwnPropertyNames(e) : null) }],
+      });
+    }
+  },
+);
+
+async function postDeleteMany(ids) {
+  const response = {};
+  for (let i = 0; i < ids.length; i++) {
+    const id = ids[i];
+    try {
+      const result = await deleteDecision(id);
+      if (result === true) {
+        response[id] = true;
+      } else {
+        response[id] = false;
+      }
+    } catch (e) {
+      response[id] = false;
+      console.error(`${process.env.APP_ID}: Error in '${route}Many' API while processing decision ${id}`);
+      console.error(e);
+    }
+  }
+  return response;
+}
+
 async function postDelete(query) {
   let response = {};
   if (query && query.id) {
@@ -46,14 +100,6 @@ async function postDelete(query) {
       const result = await deleteDecision(query.id);
       if (result === true) {
         response.deleted = true;
-        /*
-        try {
-          await Elastic.client.indices.refresh({ index: process.env.ELASTIC_INDEX });
-        } catch (e) {
-          console.error(`${process.env.APP_ID}: Error in '${route}' API while refreshing indices.`);
-          console.error(e);
-        }
-        */
       } else {
         response.deleted = false;
         response.reason = result;
@@ -74,7 +120,6 @@ async function deleteDecision(id) {
     index: process.env.ELASTIC_INDEX,
     refresh: true,
   });
-
   if (response && response.body && response.body.result === 'deleted') {
     return true;
   }
