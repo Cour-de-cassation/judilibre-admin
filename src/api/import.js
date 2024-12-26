acd2bf13590dc8e8734af3c4d4e11fc7b0bfa0e7
Julien Grach <julien.grach@justice.fr>
2024-12-26 14:47:34 +0100

    replace express schema by joi schema

--- src/api/import.js
+++ src/api/import.js
@@ -1,278 +1,127 @@
 require('../modules/env');
 const express = require('express');
 const api = express.Router();
-const { checkSchema, validationResult } = require('express-validator');
 const { toPublish } = require('../services/decision');
-
+const joi = require('joi');
 
 const route = 'import';
 
-api.post(
-  `/${route}`,
-  checkSchema({
-    id: {
-      in: 'body',
-      isString: true,
-      errorMessage: `Request has no id.`,
-      optional: false,
-    },
-    decisions: {
-      in: 'body',
-      isArray: true,
-      notEmpty: true,
-      errorMessage: `Request has no decision.`,
-      optional: false,
-    },
-    'decisions.*.chamber': {
-      in: 'body',
-      isString: true,
-      errorMessage: `Decision has no chamber.`,
-      optional: true,
-    },
-    'decisions.*.decision_date': {
-      in: 'body',
-      isISO8601: true,
-      errorMessage: `Decision has no date.`,
-      optional: false,
-    },
-    'decisions.*.id': {
-      in: 'body',
-      isString: true,
-      errorMessage: `Decision has no id.`,
-      optional: false,
-    },
-    'decisions.*.version': {
-      in: 'body',
-      isFloat: true,
-      errorMessage: `Decision has no version number.`,
-      optional: true,
-    },
-    'decisions.*.source': {
-      in: 'body',
-      isString: true,
-      errorMessage: `Decision has no source.`,
-      optional: false,
-    },
-    'decisions.*.sourceId': {
-      in: 'body',
-      isString: true,
-      errorMessage: `Decision has no sourceId.`,
-      optional: false,
-    },
-    'decisions.*.jurisdiction': {
-      in: 'body',
-      isString: true,
-      errorMessage: `Decision has no jurisdiction.`,
-      optional: false,
-    },
-    'decisions.*.number': {
-      in: 'body',
-      toArray: true,
-    },
-    'decisions.*.number.*': {
-      in: 'body',
-      isString: true,
-      errorMessage: `Decision.number must be an array of strings.`,
-      optional: false,
-    },
-    'decisions.*.publication': {
-      in: 'body',
-      toArray: true,
-    },
-    'decisions.*.publication.*': {
-      in: 'body',
-      isString: true,
-      toLowerCase: true,
-      errorMessage: `Decision.publication must be an array of strings.`,
-      optional: true,
-    },
-    'decisions.*.solution': {
-      in: 'body',
-      isString: true,
-      errorMessage: `Decision has no solution.`,
-      optional: true,
-    },
-    'decisions.*.type': {
-      in: 'body',
-      isString: true,
-      errorMessage: `Decision has no type.`,
-      optional: true,
-    },
-    'decisions.*.text': {
-      in: 'body',
-      isString: true,
-      errorMessage: `Decision has no text.`,
-      optional: false,
-    },
-    'decisions.*.displayText': {
-      in: 'body',
-      isString: true,
-      errorMessage: `Decision has no display text.`,
-      optional: false,
-    },
-    'decisions.*.location': {
-      in: 'body',
-      isString: true,
-      errorMessage: `Decision.location must be a string.`,
-      optional: true,
-    },
-    'decisions.*.ecli': {
-      in: 'body',
-      isString: true,
-      errorMessage: `Decision.ecli must be a string.`,
-      optional: true,
-    },
-    'decisions.*.formation': {
-      in: 'body',
-      isString: true,
-      errorMessage: `Decision.formation must be a string.`,
-      optional: true,
-    },
-    'decisions.*.zones': {
-      in: 'body',
-      isObject: true,
-      errorMessage: `Decision.zones must be an object.`,
-      optional: true,
-    },
-    'decisions.*.nac': {
-      in: 'body',
-      isString: true,
-      errorMessage: `Decision.nac must be a string.`,
-      optional: true,
-    },
-    'decisions.*.portalis': {
-      in: 'body',
-      isString: true,
-      errorMessage: `Decision.portalis must be a string.`,
-      optional: true,
-    },
-    'decisions.*.update_date': {
-      in: 'body',
-      isISO8601: true,
-      errorMessage: `Decision.update_date must be a ISO-8601 date (e.g. 2021-05-13).`,
-      optional: true,
-    },
-    'decisions.*.visa': {
-      in: 'body',
-      isArray: true,
-      errorMessage: `Decision.visa must be an array.`,
-      optional: true,
-    },
-    'decisions.*.rapprochements': {
-      in: 'body',
-      isArray: true,
-      errorMessage: `Decision.rapprochements must be an array.`,
-      optional: true,
-    },
-    'decisions.*.contested': {
-      in: 'body',
-      isObject: true,
-      errorMessage: `Decision.contested must be an object.`,
-      optional: true,
-    },
-    'decisions.*.forward': {
-      in: 'body',
-      isObject: true,
-      errorMessage: `Decision.forward must be an object.`,
-      optional: true,
-    },
-    'decisions.*.timeline': {
-      in: 'body',
-      isArray: true,
-      errorMessage: `Decision.timeline must be an array.`,
-      optional: true,
-    },
-    'decisions.*.solution_alt': {
-      in: 'body',
-      isString: true,
-      errorMessage: `Decision.solution_alt must be a string.`,
-      optional: true,
-    },
-    'decisions.*.summary': {
-      in: 'body',
-      isString: true,
-      errorMessage: `Decision.summary must be a string.`,
-      optional: true,
-    },
-    'decisions.*.bulletin': {
-      in: 'body',
-      isString: true,
-      errorMessage: `Decision.bulletin must be a string.`,
-      optional: true,
-    },
-    'decisions.*.files': {
-      in: 'body',
-      isArray: true,
-      errorMessage: `Decision.files must be an array.`,
-      optional: true,
-    },
-    'decisions.*.themes': {
-      in: 'body',
-      isArray: true,
-      errorMessage: `Decision.themes must be an array.`,
-      optional: true,
-    },
-    'decisions.*.lowInterest': {
-      in: 'body',
-      isBoolean: true,
-      toBoolean: true,
-      errorMessage: `Decision.lowInterest must be a boolean.`,
-      optional: true,
-    },
-    'decisions.*.partial': {
-      in: 'body',
-      isBoolean: true,
-      toBoolean: true,
-      errorMessage: `Decision.partial must be a boolean.`,
-      optional: true,
-    },
-    'decisions.*.legacy': {
-      in: 'body',
-      isObject: true,
-      errorMessage: `Decision.legacy must be an object.`,
-      optional: true,
-    },
-    'decisions.*.decision_datetime': {
-      in: 'body',
-      isISO8601: true,
-      errorMessage: `Decision has no datetime.`,
-      optional: false,
-    },
-    'decisions.*.update_datetime': {
-      in: 'body',
-      isISO8601: true,
-      errorMessage: `Decision.update_datetime must be a ISO-8601 full date (e.g. 2021-05-13T06:00:00Z).`,
-      optional: true,
-    },
-    'decisions.*.titlesAndSummaries': {
-      in: 'body',
-      isArray: true,
-      errorMessage: `Decision.titlesAndSummaries must be an Array.`,
-      optional: true,
-    },
-    'decisions.*.particularInterest': {
-      in: 'body',
-      isBoolean: true,
-      toBoolean: true,
-      errorMessage: `Decision.particularInterest must be a boolean.`,
-      optional: true,
-    },
+const zoneSchema = joi.array().items(
+  joi.object({
+    start: joi.number().precision(0).required(),
+    end: joi.number().precision(0).required(),
   }),
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ route: `${req.method} ${req.path}`, errors: errors.array() });
-    }
-    try {
-      const result = await toPublish(req.body.decisions);
-      return res.status(200).json(result);
-    } catch (e) {
-      return res.status(500).json({
-        route: `${req.method} ${req.path}`,
-        errors: [{ msg: 'Internal Server Error', error: JSON.stringify(e, e ? Object.getOwnPropertyNames(e) : null) }],
-      });
-    }
-  },
 );
+
+const importInputSchema = joi.object({
+  id: joi.string().required().messages({ id: 'Request has no id.' }),
+  decisions: joi
+    .array()
+    .min(1)
+    .items(
+      joi.object({
+        chamber: joi.string().messages({ 'decision.chamber': 'Decision chamber must be a string.' }),
+        decision_date: joi
+          .string()
+          .isoDate()
+          .required()
+          .messages({ 'decision.decision_date': 'Decision has no date.' }),
+        id: joi.string().required().messages({ 'decision.id': 'Decision has no id.' }),
+        version: joi.number().messages({ 'decision.version': 'Decision version number must be a number.' }),
+        source: joi.string().required().messages({ 'decision.source': 'Decision has no source.' }),
+        sourceId: joi.string().required().messages({ 'decision.sourceId': 'Decision has no sourceId.' }),
+        jurisdiction: joi.string().required().messages({ 'decision.jurisdiction': 'Decision has no jurisdiction.' }),
+        number: joi
+          .array()
+          .min(1)
+          .items(joi.string())
+          .required()
+          .messages({ 'decision.number': 'Decision.number must be an array of strings.' }),
+        publication: joi
+          .array()
+          .items(joi.string().lowercase())
+          .messages({ 'decision.publication': 'Decision.publication must be an array of lowercase strings.' }),
+        solution: joi.string().messages({ 'decision.solution': 'Decision solution must be a string.' }),
+        type: joi.string().messages({ 'decision.type': 'Decision type must be a string.' }),
+        text: joi.string().required().messages({ 'decision.text': 'Decision has no text.' }),
+        displayText: joi.string().required().messages({ 'decision.displayText': 'Decision has no display text.' }),
+        location: joi.string().messages({ 'decision.location': 'Decision.location must be a string.' }),
+        ecli: joi.string().messages({ 'decision.ecli': 'Decision.ecli must be a string.' }),
+        formation: joi.string().messages({ 'decision.formation': 'Decision.formation must be a string.' }),
+        zones: joi.object({
+          introduction: zoneSchema.messages({
+            'decision.introduction':
+              'Decision.zones.introduction must be an array of including start and end as integer.',
+          }),
+          expose: zoneSchema.messages({
+            'decision.expose': 'Decision.zones.expose must be an array of including start and end as integer.',
+          }),
+          moyens: zoneSchema.messages({
+            'decision.moyens': 'Decision.zones.moyens must be an array of including start and end as integer.',
+          }),
+          motivations: zoneSchema.messages({
+            'decision.motivations': 'Decision.zones.motivations must be an array of including start and end as integer.',
+          }),
+          dispositif: zoneSchema.messages({
+            'decision.dispositif': 'Decision.zones.dispositif must be an array of including start and end as integer.',
+          }),
+          annexes: zoneSchema.messages({
+            'decision.annexes': 'Decision.zones.annexes must be an array of including start and end as integer.',
+          }),
+        }),
+        nac: joi.string().messages({ 'decision.nac': 'Decision.nac must be a string.' }),
+        portalis: joi.string().messages({ 'decision.portalis': 'Decision.portalis must be a string.' }),
+        update_date: joi
+          .string()
+          .isoDate()
+          .messages({ 'decision.update_date': 'Decision.update_date must be a ISO-8601 date (e.g. 2021-05-13).' }), // example is not iso date
+        visa: joi.array(),
+        rapprochements: joi.array(),
+        contested: joi.object(),
+        forward: joi.object(),
+        timeline: joi.array(),
+        solution_alt: joi.string().messages({ 'decision.solution_alt': 'Decision.solution_alt must be a string.' }),
+        summary: joi.string().messages({ 'decision.summary': 'Decision.summary must be a string.' }),
+        bulletin: joi.string().messages({ 'decision.bulletin': 'Decision.bulletin must be a string.' }),
+        files: joi.array(),
+        themes: joi.array(),
+        lowInterest: joi.boolean().messages({ 'decision.lowInterest': 'Decision.lowInterest must be a boolean.' }), // toBoolean: true ?
+        partial: joi.boolean().messages({ 'decision.partial': 'Decision.partial must be a boolean.' }), // toBoolean: true ?
+        legacy: joi.object(),
+        decision_datetime: joi
+          .string()
+          .isoDate()
+          .required()
+          .messages({ 'decision.decision_datetime': 'Decision has no datetime.' }),
+        update_datetime: joi
+          .string()
+          .isoDate()
+          .messages({
+            'decision.update_datetime':
+              'Decision.update_datetime must be a ISO-8601 full date (e.g. 2021-05-13T06:00:00Z).',
+          }),
+        titlesAndSummaries: joi.array(),
+        particularInterest: joi
+          .boolean()
+          .messages({ 'decision.particularInterest': 'Decision.particularInterest must be a boolean.' }),
+      }),
+    )
+    .required()
+    .messages({ 'decisions': 'Request has no decision'}),
+});
+
+api.post(`/${route}`, async (req, res) => {
+  try {
+    const body = await importInputSchema.validateAsync(req.body, { abortEarly: false });
+    const result = await toPublish(body);
+    return res.status(200).json(result);
+  } catch (e) {
+    if (joi.isError(e)) return res.status(400).json({ route: `${req.method} ${req.path}`, errors: e });
+    return res.status(500).json({
+      route: `${req.method} ${req.path}`,
+      errors: [{ msg: 'Internal Server Error', error: JSON.stringify(e, e ? Object.getOwnPropertyNames(e) : null) }],
+    });
+  }
+});
 
 module.exports = api;
